ad3dd6525064599d539557ee7e3e3d7f751bf29c
Prohor Vasilyev <i7poxop@mail.ru>
2018-01-12 04:59:33 +0300

    Задачка по строковым кавычкам

--- task/script.js
+++ task/script.js
@@ -1,13 +1,3 @@
-/*Task 1*/
-
-let admin, name;
-name = "John";
-admin = name;
-alert( admin );
-
-/*Task 2*/
-let currentUserName = "John";
-let name
-/*Task 3*/
-const birthday = '18.04.1982';
-const age = someCode(birthday);
+hello 1
+hello name
+hello Ilya
