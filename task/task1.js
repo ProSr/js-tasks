cb3170b7edeab6b98f120a8d4d021137abb53281
ProSr <i7poxop@mail.ru>
2018-02-01 20:35:34 +0300

    About JSLint & JSHint

--- task/task1.js
+++ task/task1.js
@@ -1,8 +1,46 @@
+'use strict';
 /*Task1*/
+
+//ESLint
 
 /*
 
-И Сегодня без задач
+function pow(x,n)
+{
+    let result=1;
+    for(let i=0;i<n;i++) {result*=x;}
+    return result;
+}
+
+let x=prompt('x?',''), n=prompt('n?','');
+if (n<=0)
+{
+    alert(`Power ${n} is not supported, please enter an integer 
+    number greater than zero`);
+}
+else
+{
+    alert(pow(x,n));
+}
 
 */
+
+//JSLint;
+function pow(x, n) {
+  let result = 1, i;
+  for (i = 0; i < n; i++) {result *= x; }
+  return result;
+}
+
+let x /*= prompt("x?", '')*/, n /*= prompt("n?", '')*/;
+if (n <= 0) {
+  alert(`Power ${n} is not supported, please enter an 
+  integer number greater than zero`);
+}
+else
+{
+  alert(pow(x,n))
+}
+// В заключении, JSLint удобен, встроен в редактор, но пока ещё не настроен как следует.
+//ESLint нужно инсталировать каждый раз в проект, он не обращает внимания на детали, а только на грубые ошибки.
 //Done +++
