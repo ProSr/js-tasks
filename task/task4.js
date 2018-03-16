720ea1c2766727b00291c07186c14b904b67d82d
ProSr <i7poxop@mail.ru>
2018-03-16 06:16:21 +0300

    No tasks. Chapter about old var

--- task/task4.js
+++
@@ -1,7 +0,0 @@
-//Task 4. Sum with closures
-function sum(a) {
-  return function(b) {
-    return a + b
-  }
-}
-//done
--- task/task6.js
+++
@@ -1,6 +0,0 @@
-//Task 6. Sort by field
-function byField(x) {
-  return (a, b) => a[x] > b[x] ? 1 : -1;
-}
-
-//done
