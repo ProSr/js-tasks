c1f493aeb9361c16b997bb2241745233ecb37992
ProSr <i7poxop@mail.ru>
2018-06-02 23:52:43 +0300

    Mouse events basics. one task complete

--- task/mocha/test.js
+++ task/mocha/test.js
@@ -1,72 +1,25 @@
 /* eslint-disable no-undef */
 describe("Task 1", function() {
-	context("Why \"return false\" doesn't work?", function() {
-		before(function(){
+	context("Selectable list", function() {
+	  	before(function(){
 			auxiliaryHTMLtask1();
-			window.handler = solutionTask1();
-			window.alert = sinon.stub(window, "alert");
+			solutionTask1();
+			
 		});
 		after(function() {
 			divTask1.remove();
-			window.alert.restore();
-			window.handler = null;
 		});
-		it("Task solution is ' . preventDefault ( ) '", function() {
-			divTask1.lastElementChild.dispatchEvent(new Event('click'));
-			assert(alert.calledWith('. . .'));
+		it("document.querySelectorAll('.selected').length, 1", function() {
+			ul.firstElementChild.dispatchEvent(new MouseEvent('click', {bubbles: true, metaKey: true, ctrlKey: true}))
+			assert.equal(document.querySelectorAll('.selected').length, 1);
 		});
-	});
-});
-describe("Task 2", function() {
-	context("Catch links in the element", function() {
-		before(function(){
-			window.confirm = sinon.stub(window, "confirm");
-			confirm.returns(true);
-			auxiliaryHTMLtask2();
-			solutionTask2();
+		it("document.querySelectorAll('.selected').length, 2", function() {
+			ul.lastElementChild.dispatchEvent(new MouseEvent('click', {bubbles: true, metaKey: true, ctrlKey: true}))
+			assert.equal(document.querySelectorAll('.selected').length, 2);
 		});
-		after(function() {
-			divTask2.remove();
-			window.confirm.restore();
-		});
-		it("At this task dispatching not work", function() {
-			contents.querySelectorAll('a')[1].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}));
-			confirm.returns(true);
-			assert.equal(confirm('asdf'), 0);
-		});
-	});
-});
-describe("Task 3", function() {
-	context("Image gallery", function() {
-		before(function(){
-			auxiliaryHTMLtask3();
-			solutionTask3();
-		});
-		after(function() {
-			divTask3.remove();
-		});
-		it("largeImg.src === 'https://en.js.cx/gallery/img1-lg.jpg'", function() {
-			assert.equal(largeImg.src, 'https://en.js.cx/gallery/img1-lg.jpg');
-		});
-		it("largeImg.src === 'https://en.js.cx/gallery/img1-lg.jpg' after divTask3.lastElementChild.children[0].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}))", function() {
-			divTask3.lastElementChild.children[0].firstElementChild.firstElementChild.dispatchEvent(new Event('click', {bubbles: true}));
-			assert.equal(largeImg.src, 'https://en.js.cx/gallery/img2-lg.jpg');
-		});
-		it("largeImg.src === 'https://en.js.cx/gallery/img1-lg.jpg' after divTask3.lastElementChild.children[1].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}))", function() {
-			divTask3.lastElementChild.children[1].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}));
-			assert.equal(largeImg.src, 'https://en.js.cx/gallery/img3-lg.jpg');
-		});
-		it("largeImg.src === 'https://en.js.cx/gallery/img1-lg.jpg' after divTask3.lastElementChild.children[2].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}))", function() {
-			divTask3.lastElementChild.children[2].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}));
-			assert.equal(largeImg.src, 'https://en.js.cx/gallery/img4-lg.jpg');
-		});
-		it("largeImg.src === 'https://en.js.cx/gallery/img1-lg.jpg' after divTask3.lastElementChild.children[3].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}))", function() {
-			divTask3.lastElementChild.children[3].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}));
-			assert.equal(largeImg.src, 'https://en.js.cx/gallery/img5-lg.jpg');
-		});
-		it("largeImg.src === 'https://en.js.cx/gallery/img1-lg.jpg' after divTask3.lastElementChild.children[4].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}))", function() {
-			divTask3.lastElementChild.children[4].firstElementChild.dispatchEvent(new Event('click', {bubbles: true}));
-			assert.equal(largeImg.src, 'https://en.js.cx/gallery/img6-lg.jpg');
+		it("document.querySelectorAll('.selected').length, 3", function() {
+			ul.children[3].dispatchEvent(new MouseEvent('click', {bubbles: true, metaKey: true, ctrlKey: true}))
+			assert.equal(document.querySelectorAll('.selected').length, 3);
 		});
 	});
 });
--- task/task1.js
+++ task/task1.js
@@ -1,20 +1,33 @@
-// Task 1. Why "return false" doesn't work?
+// Task 1. Selectable list
 function auxiliaryHTMLtask1() {
-	let divTask1 = document.createElement('div');
-	divTask1.id = 'divTask1';
-	divTask1.innerHTML = `<a href="http://w3.org" onclick="handler(event)">the browser will go to w3.org</a>`;
-	document.body.prepend(divTask1);
+	let d = document.createElement('div');
+	d.innerHTML = `<style>
+		.selected {
+		  background: #0f0;
+		}
+
+		li {
+		  cursor: pointer;
+		}
+	</style>`;
+	d.innerHTML += `Click on a list item to select it.<br><ul id="ul"><li>Christopher Robin</li><li>Winnie-the-Pooh</li><li>Tigger</li><li>Kanga</li><li>Rabbit. Just rabbit.</li></ul>`;
+	d.id = 'divTask1';
+	document.body.prepend(d);
+/*
+	ul.style.userSelect = 'none'; // First solution. it works
+*/
 }
-
-handler = function(e) {
-	alert( "..." );
-	return false;
-};
-
 function solutionTask1() {
-	return function(e) {
-		alert( ". . ." );
-		e.preventDefault();
-	}
+	ul.onmousedown = e => e.preventDefault();  // Third solution. it works. It's the best solution
+	ul.onclick = function(e) {
+/*
+		getSelection().removeAllRanges(); // Second solution. it works
+*/
+		if(e.ctrlKey || e.metaKey) e.target.classList.toggle('selected');
+		else {
+			[...e.currentTarget.children].forEach(item => item.classList.remove('selected'));
+			e.target.classList.toggle('selected');
+		}
+	};
 }
 // Done.
