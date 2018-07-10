7de0778254bd97dc0212ee55fa3c88d9c8a9aa1b
ProSr <i7poxop@mail.ru>
2018-07-10 10:53:02 +0300

    Keyboard: keydown and keyup .One task

--- task/mocha/test.js
+++ task/mocha/test.js
@@ -1,121 +1,22 @@
 /* eslint-disable no-undef */
-describe("Task 1", function() {
-    context("Slider", function() {
-
-        let thumb,
-			coords;
-
+describe.only("Task 1", function() {
+    context("Extended hotkeys", function() {
 
         before(function() {
-            auxiliaryHTMLtask1();
             solutionTask1();
+			window.alert = sinon.stub(window, "alert");
         });
         after(function() {
-            divTask1.remove();
+			document.onkeydown = '';
+			document.onkeyup = '';
+			solutionTask = '';
+			window.alert.restore();
         });
-        beforeEach(function() {
-            thumb = document.querySelector('.thumb');
-			coords = thumb.getBoundingClientRect();
-        })
-        it('thumb . getBoundingClientRect ( ) . left = 65 move right at the 135 px. thumb . getBoundingClientRect ( ) . left = 200', function() {
-			thumb.dispatchEvent(new MouseEvent('mousedown', {clientX: 65, pageX: 65, clientY: 70, pageY: 70, bubbles: true}));
-			document.dispatchEvent(new MouseEvent('mousemove', {clientX: 200, clientY: 200, pageX: 200, pageY: 200, bubbles: true}));
-			document.dispatchEvent(new MouseEvent('mouseup', {clientX: 200, clientY: 200, pageX: 200, pageY: 200, bubbles: true}));
-			
-            assert.equal(thumb.getBoundingClientRect().left, 200);
-        });
-        it('The "thumb" element have to hold position AT THE LEFT', function() {
-			thumb.dispatchEvent(new MouseEvent('mousedown', {clientX: 200, clientY: 70, pageX: 200, pageY: 70, bubbles: true}));
-			document.dispatchEvent(new MouseEvent('mousemove', {clientX: 20, clientY: 200, pageX: 20, pageY: 200, bubbles: true}));
-			document.dispatchEvent(new MouseEvent('mouseup', {clientX: 200, clientY: 200, pageX: 200, pageY: 200, bubbles: true}));
-			
-            assert.equal(thumb.getBoundingClientRect().left, slider.getBoundingClientRect().left);
-        });
-        it('The "thumb" element have to hold position AT THE RIGHT', function() {
-			thumb.dispatchEvent(new MouseEvent('mousedown', {clientX: slider.getBoundingClientRect().left, clientY: 70, pageX: slider.getBoundingClientRect().left, pageY: 70, bubbles: true}));
-			document.dispatchEvent(new MouseEvent('mousemove', {clientX: 500, clientY: 200, pageX: 500, pageY: 200, bubbles: true}));
-			document.dispatchEvent(new MouseEvent('mouseup', {clientX: 200, clientY: 200, pageX: 200, pageY: 200, bubbles: true}));
-			
-            assert.equal(thumb.getBoundingClientRect().left, slider.getBoundingClientRect().right - thumb.offsetWidth);
+        
+        it('alert from "func" function, returns keywords "KeyQ,KeyW" after keydown "Q" + "W" combination', function() {
+			document.dispatchEvent(new KeyboardEvent('keydown', {bubbles: true, code: 'KeyQ'}));		
+			document.dispatchEvent(new KeyboardEvent('keydown', {bubbles: true, code: 'KeyW'}));
+            assert(alert.calledWith("KeyQ,KeyW"));
         });
     });
 });
-describe("Task 2", function() {
-    context("Drag superheroes around the field", function() {
-        before(function() {
-            auxiliaryHTMLtask2();
-            solutionTask2();
-        });
-
-        after(function() {
-			divTask2.remove();
-        });
-
-        it("set the Ball at the field", function() {
-			let img = divTask2.querySelector('img'),
-				imgRect = img.getBoundingClientRect();
-			img.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, clientX: imgRect.left + img.offsetWidth / 2, clientY: imgRect.top + img.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			img.dispatchEvent(new MouseEvent('mousemove', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 2, clientY: field.getBoundingClientRect().top + field.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			img.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 2, clientY: field.getBoundingClientRect().top + field.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			
-            assert(img.getBoundingClientRect().left < field.getBoundingClientRect().right / 1.8);
-        });
-        it("set the Hero1 at the field", function() {
-			let hero = divTask2.querySelector('#hero1'),
-				heroRect = hero.getBoundingClientRect()
-			hero.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, clientX: heroRect.left + hero.offsetWidth / 2, clientY: heroRect.top + hero.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mousemove', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 3, clientY: field.getBoundingClientRect().top + field.offsetHeight / 3, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 3, clientY: field.getBoundingClientRect().top + field.offsetHeight / 3, pageX: this.clientX, pageY: this.clientY }));
-			
-            assert(hero.getBoundingClientRect().left < field.getBoundingClientRect().right / 2);
-        });
-        it("set the Hero2 at the field", function() {
-			let hero = divTask2.querySelector('#hero2'),
-				heroRect = hero.getBoundingClientRect()
-			hero.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, clientX: heroRect.left + hero.offsetWidth / 2, clientY: heroRect.top + hero.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mousemove', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 3, clientY: field.getBoundingClientRect().top + field.offsetHeight * 0.8, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 3, clientY: field.getBoundingClientRect().top + field.offsetHeight / 3, pageX: this.clientX, pageY: this.clientY }));
-			
-            assert(hero.getBoundingClientRect().left < field.getBoundingClientRect().right / 2);
-        });
-        it("set the Hero3 at the field", function() {
-			let hero = divTask2.querySelector('#hero3'),
-				heroRect = hero.getBoundingClientRect()
-			hero.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, clientX: heroRect.left + hero.offsetWidth / 2, clientY: heroRect.top + hero.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mousemove', { bubbles: true, clientX: field.getBoundingClientRect().right - field.offsetWidth / 3, clientY: field.getBoundingClientRect().top + field.offsetHeight / 3, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 3, clientY: field.getBoundingClientRect().top + field.offsetHeight / 3, pageX: this.clientX, pageY: this.clientY }));
-			
-            assert(hero.getBoundingClientRect().left > field.getBoundingClientRect().right / 2);
-        });
-        it("set the Hero4 at the field", function() {
-			let hero = divTask2.querySelector('#hero4'),
-				heroRect = hero.getBoundingClientRect()
-			hero.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, clientX: heroRect.left + hero.offsetWidth / 2, clientY: heroRect.top + hero.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mousemove', { bubbles: true, clientX: field.getBoundingClientRect().right - field.offsetWidth / 3, clientY: field.getBoundingClientRect().top + field.offsetHeight * 0.8, pageX: this.clientX, pageY: this.clientY }))
-			hero.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 3, clientY: field.getBoundingClientRect().top + field.offsetHeight / 3, pageX: this.clientX, pageY: this.clientY }))
-			
-            assert(hero.getBoundingClientRect().left > field.getBoundingClientRect().right / 2);
-        });
-        it("set the Hero5 at the field", function() {
-			let hero = divTask2.querySelector('#hero5'),
-				heroRect = hero.getBoundingClientRect()
-			hero.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, clientX: heroRect.left + hero.offsetWidth / 2, clientY: heroRect.top + hero.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mousemove', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 8, clientY: field.getBoundingClientRect().top + field.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }))
-			hero.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 2, clientY: field.getBoundingClientRect().top + field.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }))
-			
-            assert(hero.getBoundingClientRect().left < field.getBoundingClientRect().right / 2);
-        });
-        it("set the Hero6 at the field", function() {
-			let hero = divTask2.querySelector('#hero6'),
-				heroRect = hero.getBoundingClientRect()
-			hero.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, clientX: heroRect.left + hero.offsetWidth / 2, clientY: heroRect.top + hero.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mousemove', { bubbles: true, clientX: field.getBoundingClientRect().right - field.offsetWidth / 8, clientY: field.getBoundingClientRect().top + field.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			hero.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, clientX: field.getBoundingClientRect().left + field.offsetWidth / 2, clientY: field.getBoundingClientRect().top + field.offsetHeight / 2, pageX: this.clientX, pageY: this.clientY }));
-			
-            assert(hero.getBoundingClientRect().left > field.getBoundingClientRect().right / 2);
-        });
-
-
-    });
-});
-
--- task/task1.js
+++ task/task1.js
@@ -1,172 +1,23 @@
-// Task 1. Slider
-function auxiliaryHTMLtask1() {
-  let divTask1 = document.createElement('div');
-  divTask1.id = 'divTask1';
-  
-  divTask1.innerHTML = `<style>
-    .slider {
-      border-radius: 5px;
-      background: #E0E0E0;
-      background: linear-gradient(left top, #E0E0E0, #EEEEEE);
-      width: 310px;
-      height: 15px;
-      margin: 5px;
-    }
-    .thumb {
-      width: 10px;
-      height: 25px;
-      border-radius: 3px;
-      position: relative;
-      left: 10px;
-      top: -5px;
-      background: blue;
-      cursor: pointer;
-    }
-  </style><div id="slider" class="slider"><div class="thumb"></div></div>`;
-  document.body.prepend(divTask1);
-}
+// Task 1. Extended hotkeys
 function solutionTask1() {
-	kantorSolution();
-//	myFirstSolution();
-//	mySecondSolution();
-	
-	function mySecondSolution() {
-		 let currentDroppable = null;
-
-		let thumb = document.querySelector('.thumb');
-
-		thumb.onmousedown = function(event) {
-
-		  let shiftX = event.clientX - thumb.getBoundingClientRect().left,
-			  sliderGBCR = slider.getBoundingClientRect(),
-			  eventClientY = (sliderGBCR.bottom - sliderGBCR.top) / 2;
-
-		  moveAt(event.pageX);
-
-		  function moveAt(pageX) {
-			thumb.style.left = pageX - shiftX - sliderGBCR.left + 'px';
-		  }
-
-		  function onMouseMove(event) {
-			if(checkState(event)) moveAt(event.pageX);
-			thumb.hidden = true;
-			let elemBelow = document.elementFromPoint(event.clientX, eventClientY);
-			thumb.hidden = false;
-
-			if (!elemBelow) return;
-
-			let droppableBelow = elemBelow.closest('.slider');
-			if (currentDroppable != droppableBelow) {
-			  if (currentDroppable) {
-				leaveDroppable(currentDroppable);
-			  }
-			  currentDroppable = droppableBelow;
-			  if (currentDroppable) {
-				enterDroppable(currentDroppable);
-			  }
-			}
-		  }
-
-		  document.addEventListener('mousemove', onMouseMove);
-
-		  document.onmouseup = function() {
-			document.removeEventListener('mousemove', onMouseMove);
-			thumb.onmouseup = null;
-		  }
-		  function checkState(e) {
-			let t = thumb.getBoundingClientRect();
-			if(t.left <= sliderGBCR.left - shiftX) {
-			  moveAt(0);
-			  return false;
-			} else if(t.right - shiftX >= sliderGBCR.right) {
-			  moveAt(sliderGBCR.right - sliderGBCR.left - thumb.offsetWidth);
-			  return false;
-			} else return true;
-		  }
-		};
-		function leaveDroppable(elem) {
-
-		};
-		function enterDroppable(elem) {
-
-		};
-		thumb.ondragstart = function() {
-		  return false;
-		};
-
-
+	let keysArray = [];
+	document.onkeydown = keyDown;	
+	document.onkeyup = keyUp;
+	function keyDown(event) {
+		keysArray.push(event.code);
+		runOnKeys(func, keysArray);
 	}
-	function myFirstSolution() {
-    
-		document.querySelector('.thumb').onmousedown = function(e, thumb = document.querySelector('.thumb')) {
-		  event.preventDefault();
-		  let shiftX = e.clientX - thumb.getBoundingClientRect().left,
-			  s = slider.getBoundingClientRect();
-		  checkState(e);
-
-		  function checkState(e) {
-			let t = thumb.getBoundingClientRect();
-			if(t.left < s.left - shiftX) moveAt(0);
-			else if(t.right - shiftX > s.right) moveAt(s.right - s.left - thumb.offsetWidth);
-			else moveAt(e.pageX - s.left - shiftX);
-		  }
-
-		  function moveAt(pageX) {
-			thumb.style.left = pageX + 'px';
-		  }
-
-		  document.onmousemove = function(e, t = thumb.getBoundingClientRect()) {
-			if(!(t.left < s.left - shiftX) && !(t.right - shiftX > s.right)) checkState(e);
-		  };
-
-		  document.onmouseup = function() {
-			document.onmousemove = '';
-			document.onmouseup = '';
-		  };
-		};
-		thumb.ondragstart = function() {
-		  return false;
-		};
-
+	function keyUp() {
+		keysArray = [];
 	}
-	function kantorSolution() {
-		
-		let thumb = slider.querySelector('.thumb');
-
-		thumb.onmousedown = function(event) {
-		  event.preventDefault(); // prevent selection start (browser action)
-
-		  let shiftX = event.clientX - thumb.getBoundingClientRect().left;
-		  // shiftY not needed, the thumb moves only horizontally
-
-		  document.addEventListener('mousemove', onMouseMove);
-		  document.addEventListener('mouseup', onMouseUp);
-
-		  function onMouseMove(event) {
-			let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
-
-			// the pointer is out of slider => lock the thumb within the bounaries
-			if (newLeft < 0) {
-			  newLeft = 0;
-			}
-			let rightEdge = slider.offsetWidth - thumb.offsetWidth;
-			if (newLeft > rightEdge) {
-			  newLeft = rightEdge;
-			}
-
-			thumb.style.left = newLeft + 'px';
-		  }
-
-		  function onMouseUp() {
-			document.removeEventListener('mouseup', onMouseUp);
-			document.removeEventListener('mousemove', onMouseMove);
-		  }
-
-		};
-
-		thumb.ondragstart = function() {
-		  return false;
-		};
+	function runOnKeys(func, code_n) {
+		if(code_n.includes('KeyQ') && code_n.includes('KeyW')) {
+			keysArray = [];
+			func(code_n);
+		}
+	}
+	function func(...keys) {
+		alert(keys.toString());
 	}
 }
 // Done.
