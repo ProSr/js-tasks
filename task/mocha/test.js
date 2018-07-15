ec0c26a0c5a8cc88344315610b93c15ca8766533
ProSr <i7poxop@mail.ru>
2018-07-15 16:14:27 +0300

    Resource loading: onload and onerror. 1 Task (we dont know how to test asynchronous functions)

--- task/mocha/test.js
+++ task/mocha/test.js
@@ -1,86 +1,34 @@
 /* eslint-disable no-undef */
 describe("Task 1", function() {
-    context("Endless page", function() {
+    context("Load images with a callback (we don't now how to test asynchronous functions)", function() {
+        it('check \'preloadImages\' function', function() {
+			let preloadImages = solutionTask1();
+			
+			let sources = [
+			  "https://en.js.cx/images-load/1.jpg",
+			  "https://en.js.cx/images-load/2.jpg",
+			  "https://en.js.cx/images-load/4.jpg",
+			  "https://en.js.cx/images-load/3.jpg"
+			];
 
-        before(function() {
-			auxiliaryHTMLtask1();
-			solutionTask1();
+			for (let i = 0; i < sources.length; i++) {
+			  sources[i] += '?' + Math.random();
+			}
 
-        });
-        after(function() {
-			divTask1.remove();
-        });
-        
-        it('startScrollHeight !== .scrollHeight', function() {
-			let startScrollHeight = divTask1.scrollHeight;
-			divTask1.dispatchEvent(new MouseEvent('scroll'));
-			divTask1.scrollTop += 200;
+			function testLoaded() {
+			  let widthSum = 0;
+			  for (let i = 0; i < sources.length; i++) {
+				let img = document.createElement('img');
+				img.src = sources[i];
+				widthSum += img.width;
+			  }
+			  alert(widthSum);
 
-            assert.notEqual(startScrollHeight, divTask1.scrollHeight);
-        });
-    });
-});
-describe("Task 2", function() {
-    context("Up/down button", function() {
-
-        before(function() {
-			auxiliaryHTMLtask2();
-			solutionTask2();
-        });
-        after(function() {
-			divTask2.remove();
-        });
-        
-        it('after scroll down "thoTheTop.hidden === false"', function() {
+			}
 			
-			let toTheTopStart = toTheTop.hidden;
-			divTask2.scrollTop += divTask2.offsetHeight + 1;
-			divTask2.dispatchEvent(new MouseEvent('scroll'));
+			preloadImages(sources, testLoaded);
 			
-            assert.notEqual(toTheTopStart, toTheTop.hidden);
-        });
-        
-        it('after scroll up "toTheTop.hidden === true"', function() {
-			
-			let toTheTopStart = toTheTop.hidden;
-			divTask2.scrollTop = 0;
-			divTask2.dispatchEvent(new MouseEvent('scroll'));
-			
-            assert.notEqual(toTheTopStart, toTheTop.hidden);
-        });
-        
-        it('after click on toTheTop "toTheTop.hidden === true" and "scrollTop === 0"', function() {
-			divTask2.scrollTop += divTask2.offsetHeight + 1;
-			divTask2.dispatchEvent(new MouseEvent('scroll'));
-			
-			let toTheTopStart = toTheTop.hidden;
-			toTheTop.dispatchEvent(new MouseEvent('click'))
-			divTask2.dispatchEvent(new MouseEvent('scroll'));
-			
-            assert.notEqual(toTheTopStart, toTheTop.hidden);
-        });
-		
-    });
-});
-describe.only("Task 3", function() {
-    context("Load visible images", function() {
-
-        before(function() {
-			auxiliaryHTMLtask3();
-			solutionTask3();
-        });
-        after(function() {
-			divTask3.remove();
-        });
-        
-        it('after the scrolling, when the first img with data-src attribute is not visible it deta-src exists', function() {
-			
-            assert(divTask3.querySelector('img').dataset.src);
-        });
-        it('after the scrolling, when the first img with data-src attribute is visible it deta-src not exists', function() {
-			divTask3.scrollTop = divTask3.clientHeight;
-			divTask3.dispatchEvent(new MouseEvent('scroll'));
-            assert(!divTask3.querySelector('img').dataset.src);
+			assert(false);
         });
     });
 });
--- task/task1.js
+++ task/task1.js
@@ -1,23 +1,26 @@
 // Task 1. Endless page
-function auxiliaryHTMLtask1() {
-	const divTask1 = document.createElement('div');
-	divTask1.id = 'divTask1';
-	divTask1.innerHTML = '<h1>Scroll me</h1>';
-	divTask1.style.cssText = `
-				position: relative;
-				width: 500px;
-				height: 300px;
-				overflow-y: scroll;
-				padding: 10px;
-				background: rgba(255,255,255,.65);`;
-	document.body.prepend(divTask1);
-	while(document.getElementById('divTask1').lastElementChild.getBoundingClientRect().bottom < document.getElementById('divTask1').getBoundingClientRect().bottom)
-		divTask1.insertAdjacentHTML('beforeend', `<p>Date: ${new Date()}</p>`);
-}
 function solutionTask1() {
-	divTask1.addEventListener('scroll', function() {
-		if(divTask1.scrollTop + divTask1.offsetHeight > divTask1.scrollHeight - 100)
-			divTask1.insertAdjacentHTML('beforeend', `<p>Date: ${new Date()}</p>`);
-	});
+	
+	function preloadImages(sources, callback) {
+/* // solution with 'for' loop
+		for(let i = 0; i < sources.length; i++) {
+			let img = document.createElement('img');
+			img.src = sources[i];
+			img.onerror = img.onload = checkCountImg;
+		}
+*/ 
+		sources.forEach(i => {
+			let img = document.createElement('img');
+			img.src = i;
+			img.onerror = img.onload = checkCountImg;
+		});
+		
+		
+		let countLoadedImg = 0;
+		function checkCountImg() {
+				if(++countLoadedImg === sources.length) callback();
+		}
+    }
+	return preloadImages;
 }
 // Done.
